97fc2453fcd4ef5e6c58e61e1299ec9d218df4c3
abdessamadsalim <samimalim1991@gmail.com>
2024-03-26 02:39:19 -0700

    task 101 done

---
+++ 101-binary_tree_levelorder.c
@@ -0,0 +1,121 @@
+#include "binary_trees.h"
+
+levelorder_queue_t *create_node(binary_tree_t *node);
+void free_queue(levelorder_queue_t *head);
+void pint_push(binary_tree_t *node, levelorder_queue_t *head,
+		levelorder_queue_t **tail, void (*func)(int));
+void pop(levelorder_queue_t **head);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+
+/**
+ * create_node - -
+ * @node: --
+ *
+ * Return: If an error occurs, NULL.
+ *         Otherwise, a pointer to the new node.
+ */
+levelorder_queue_t *create_node(binary_tree_t *node)
+{
+	levelorder_queue_t *new;
+
+	new = malloc(sizeof(levelorder_queue_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->node = node;
+	new->next = NULL;
+
+	return (new);
+}
+
+/**
+ * free_queue - --
+ * @head: --
+ */
+void free_queue(levelorder_queue_t *head)
+{
+	levelorder_queue_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
+
+/**
+ * pint_push - --
+ * @node: --
+ * @head: --
+ * @tail: --
+ * @func: --
+ *
+ * Description: Upon malloc failure, exits with a status code of 1.
+ */
+void pint_push(binary_tree_t *node, levelorder_queue_t *head,
+		levelorder_queue_t **tail, void (*func)(int))
+{
+	levelorder_queue_t *new;
+
+	func(node->n);
+	if (node->left != NULL)
+	{
+		new = create_node(node->left);
+		if (new == NULL)
+		{
+			free_queue(head);
+			exit(1);
+		}
+		(*tail)->next = new;
+		*tail = new;
+	}
+	if (node->right != NULL)
+	{
+		new = create_node(node->right);
+		if (new == NULL)
+		{
+			free_queue(head);
+			exit(1);
+		}
+		(*tail)->next = new;
+		*tail = new;
+	}
+}
+
+/**
+ * pop - --
+ * @head: --
+ */
+void pop(levelorder_queue_t **head)
+{
+	levelorder_queue_t *tmp;
+
+	tmp = (*head)->next;
+	free(*head);
+	*head = tmp;
+}
+
+/**
+ * binary_tree_levelorder - --
+ * @tree: --
+ * @func: --
+ */
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	levelorder_queue_t *head, *tail;
+
+	if (tree == NULL || func == NULL)
+		return;
+
+	head = tail = create_node((binary_tree_t *)tree);
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		pint_push(head->node, head, &tail, func);
+		pop(&head);
+	}
+}
+
---
+++ 101-binary_tree_levelorder_recursive.c
@@ -0,0 +1,113 @@
+#include "binary_trees.h"
+
+/**
+ * struct node_s - --
+ * @node: --
+ * @next: --
+ */
+typedef struct node_s
+{
+	const binary_tree_t *node;
+	struct node_s *next;
+} ll;
+
+ll *append(ll *head, const binary_tree_t *btnode);
+void free_list(ll *head);
+ll *get_children(ll *head, const binary_tree_t *parent);
+void levels_rec(ll *head, void (*func)(int));
+
+/**
+ * binary_tree_levelorder - --
+ * @tree: --
+ * @func: --
+ */
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	ll *children = NULL;
+
+	func(tree->n);
+	children = get_children(children, tree);
+	levels_rec(children, func);
+
+	free_list(children);
+}
+
+/**
+ * levels_rec - --
+ * @head: --
+ * @func: --
+ */
+void levels_rec(ll *head, void (*func)(int))
+{
+	ll *children = NULL, *curr = NULL;
+
+	if (!head)
+		return;
+	for (curr = head; curr != NULL; curr = curr->next)
+	{
+		func(curr->node->n);
+		children = get_children(children, curr->node);
+	}
+	levels_rec(children, func);
+	free_list(children);
+}
+
+/**
+ * get_children - --
+ * @head: --
+ * @parent: --
+ * Return: Pointer to head of linked list of children.
+ */
+ll *get_children(ll *head, const binary_tree_t *parent)
+{
+	if (parent->left)
+		head = append(head, parent->left);
+	if (parent->right)
+		head = append(head, parent->right);
+	return (head);
+}
+
+/**
+ * append - --
+ * @head: --
+ * @btnode: --
+ * Return: pointer to head, or NULL on failure
+ */
+ll *append(ll *head, const binary_tree_t *btnode)
+{
+	ll *new = NULL, *last = NULL;
+
+	new = malloc(sizeof(*new));
+	if (new)
+	{
+		new->node = btnode;
+		new->next = NULL;
+		if (!head)
+			head = new;
+		else
+		{
+			last = head;
+			while (last->next)
+				last = last->next;
+			last->next = new;
+		}
+	}
+	return (head);
+}
+
+/**
+ * free_list - --
+ * @head: --
+ */
+void free_list(ll *head)
+{
+	ll *ptr = NULL;
+
+	while (head)
+	{
+		ptr = head->next;
+		free(head);
+		head = ptr;
+	}
+}
+
