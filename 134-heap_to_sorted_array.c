44297fea75b27d1b05e5b7c8f3ec34c89bbd14d6
abdessamadsalim <samimalim1991@gmail.com>
2024-03-26 03:07:53 -0700

    task 130 131 132 133 134

---
+++ 134-heap_to_sorted_array.c
@@ -0,0 +1,50 @@
+#include "binary_trees.h"
+
+/**
+ * tree_size - --
+ * @tree: --
+ * Return: Height or 0 if tree is NULL
+ */
+size_t tree_size(const binary_tree_t *tree)
+{
+	size_t height_l = 0;
+	size_t height_r = 0;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left)
+		height_l = 1 + tree_size(tree->left);
+
+	if (tree->right)
+		height_r = 1 + tree_size(tree->right);
+
+	return (height_l + height_r);
+}
+
+/**
+ * heap_to_sorted_array - --
+ * @heap: --
+ * @size: --
+ * Return: pointer to array sorted in descending order
+ **/
+int *heap_to_sorted_array(heap_t *heap, size_t *size)
+{
+	int i, *a = NULL;
+
+	if (!heap || !size)
+		return (NULL);
+
+	*size = tree_size(heap) + 1;
+
+	a = malloc(sizeof(int) * (*size));
+
+	if (!a)
+		return (NULL);
+
+	for (i = 0; heap; i++)
+		a[i] = heap_extract(&heap);
+
+	return (a);
+}
+
